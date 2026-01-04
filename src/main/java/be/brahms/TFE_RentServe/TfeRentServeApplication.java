053afface2fc3ed9a8a3b2ce0ac8456855993586
Brahms <bramso.brahms@gmail.com>
2026-01-04 17:21:30 +0100

    Initialization of the SpringBoot project

---
+++ src/main/java/be/brahms/TFE_RentServe/TfeRentServeApplication.java
@@ -0,0 +1,13 @@
+package be.brahms.TFE_RentServe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfeRentServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfeRentServeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/brahms/TFE_RentServe/TfeRentServeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.brahms.TFE_RentServe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfeRentServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
