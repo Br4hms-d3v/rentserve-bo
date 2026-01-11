b5f73c3b2a55710573ff355e2a843a42fe4674ba
Brahms <bramso.brahms@gmail.com>
2026-01-11 17:07:48 +0100

    Add method to find by pseudo

--- src/main/java/be/brahms/TFE_RentServe/repositories/UserRepository.java
+++ src/main/java/be/brahms/TFE_RentServe/repositories/UserRepository.java
@@ -2,6 +2,18 @@ package be.brahms.TFE_RentServe.repositories;
 
 import be.brahms.TFE_RentServe.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * Find a user by their pseudo (username).
+     *
+     * @param pseudo the username to search for
+     * @return an Optional containing the User if found, or empty if not
+     */
+    Optional<User> findByPseudo(String pseudo);
 }
