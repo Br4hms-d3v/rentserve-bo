6aabce8633bdd5baf4640efc0f4fb0e5d569f2cf
Brahms <bramso.brahms@gmail.com>
2026-01-12 19:55:51 +0100

    Register a new user

--- src/main/java/be/brahms/TFE_RentServe/services/UserService.java
+++ src/main/java/be/brahms/TFE_RentServe/services/UserService.java
@@ -1,4 +1,20 @@
 package be.brahms.TFE_RentServe.services;
 
+import be.brahms.TFE_RentServe.models.entities.User;
+import be.brahms.TFE_RentServe.models.forms.user.UserForm;
+import jakarta.validation.Valid;
+
+/**
+ * Service interface for managing user.
+ * Defines business operations related to Bill entities.
+ */
 public interface UserService {
+
+    /**
+     * This method saves a new user.
+     *
+     * @param user the user to register
+     * @return the saved user
+     */
+    User register(@Valid UserForm user);
 }
