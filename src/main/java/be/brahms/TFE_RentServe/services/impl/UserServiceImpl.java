95f72c65428a05bfc502a50fc38668437f6864c4
Brahms <bramso.brahms@gmail.com>
2026-01-13 22:04:14 +0100

    Add method to activate user and update Create user => add send email

--- src/main/java/be/brahms/TFE_RentServe/services/impl/UserServiceImpl.java
+++ src/main/java/be/brahms/TFE_RentServe/services/impl/UserServiceImpl.java
@@ -1,13 +1,16 @@
 package be.brahms.TFE_RentServe.services.impl;
 
 import be.brahms.TFE_RentServe.exceptions.email.EmailExistingException;
+import be.brahms.TFE_RentServe.exceptions.email.EmailNotFoundException;
 import be.brahms.TFE_RentServe.exceptions.user.PseudoExistException;
 import be.brahms.TFE_RentServe.exceptions.user.UserException;
 import be.brahms.TFE_RentServe.mappers.AuthMapper;
+import be.brahms.TFE_RentServe.models.dtos.email.EmailTokenDTO;
 import be.brahms.TFE_RentServe.models.entities.User;
 import be.brahms.TFE_RentServe.models.forms.user.UserForm;
 import be.brahms.TFE_RentServe.repositories.UserRepository;
 import be.brahms.TFE_RentServe.services.UserService;
+import be.brahms.TFE_RentServe.services.email.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -24,6 +27,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final AuthMapper authMapper;
+    private final EmailService emailService;
 
     /**
      * Constructor to create UserServiceImpl with UserRepository.
@@ -33,10 +37,11 @@ public class UserServiceImpl implements UserService {
      * @param authMapper            the mapper for user data
      */
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, AuthMapper authMapper) {
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, AuthMapper authMapper, EmailService emailService) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.authMapper = authMapper;
+        this.emailService = emailService;
     }
 
     /**
@@ -74,6 +79,33 @@ public class UserServiceImpl implements UserService {
 
         // Hash the password
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+
+        // Generate a token for email
+        EmailTokenDTO emailTokenDTO = new EmailTokenDTO();
+
+        // Send email for confirmation of registration account
+        String confirmation = "http://localhost:8080/api/mail/confirmation?token=" + emailTokenDTO.confirmationToken() + "&email=" + user.getEmail();
+
+        emailService.sendMailConfirmation(user.getEmail(), confirmation);
         return userRepository.save(user);
+    }
+
+    /**
+     * This method activate the user
+     * after click on his email.
+     * The account come True
+     *
+     * @param email The user email.
+     */
+    @Override
+    public void activateUser(String email) {
+        User userActivated = userRepository.findByEmail(email);
+
+        if (userActivated == null) {
+            throw new EmailNotFoundException();
+        }
+
+        userActivated.setIsActive(true);
+        userRepository.save(userActivated);
     }
 }
