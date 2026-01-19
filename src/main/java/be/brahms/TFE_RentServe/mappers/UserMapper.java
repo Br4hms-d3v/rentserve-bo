e686a9bed3a3b084f0db222ba02c2f2c400d103b
Brahms <bramso.brahms@gmail.com>
2026-01-19 22:59:15 +0100

    Add mapper Form to Entity

--- src/main/java/be/brahms/TFE_RentServe/mappers/UserMapper.java
+++ src/main/java/be/brahms/TFE_RentServe/mappers/UserMapper.java
@@ -1,9 +1,12 @@
 package be.brahms.TFE_RentServe.mappers;
 
 import be.brahms.TFE_RentServe.models.dtos.user.UserDTO;
+import be.brahms.TFE_RentServe.models.dtos.user.UserRoleDTO;
 import be.brahms.TFE_RentServe.models.entities.User;
+import be.brahms.TFE_RentServe.models.forms.user.UserUpdateForm;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 /**
  * Mapper for user data
@@ -12,8 +15,10 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    // Entity to Data Access Object
+
     /**
-     * Create a UserDTO from a User
+     * Map the UserDTO from a User
      *
      * @param user the user data
      * @return a User dto
@@ -29,4 +34,30 @@ public interface UserMapper {
     @Mapping(target = "zipCode", source = "zipCode")
     @Mapping(target = "isActive", source = "isActive")
     UserDTO toDto(User user);
+
+    /**
+     * Map a UserRoleDto from a user
+     *
+     * @param user the user data
+     * @return a User from role
+     */
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "firstName", source = "firstName")
+    @Mapping(target = "pseudo", source = "pseudo")
+    @Mapping(target = "role", source = "role")
+    UserRoleDTO listRoleToDto(User user);
+
+    // Form to Entity
+
+    /**
+     * Update a user from userUpdateForm
+     * Some fields are ignored.
+     *
+     * @param form the user form to update
+     * @return a user with update data
+     */
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    void fromUpdateUserForm(UserUpdateForm form, @MappingTarget User user);
+
 }
