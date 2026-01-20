06bf48a0fd25582f5ecfb565046eebca8ca408dc
Brahms <bramso.brahms@gmail.com>
2026-01-21 00:37:55 +0100

    add method to send a mail for changing password

--- src/main/java/be/brahms/TFE_RentServe/services/email/EmailService.java
+++ src/main/java/be/brahms/TFE_RentServe/services/email/EmailService.java
@@ -9,4 +9,12 @@ public interface EmailService {
      * @param urlConfirmation The URL.
      */
     void sendMailConfirmation(String to, String urlConfirmation);
+
+    /**
+     * Sends a mail to confirm the password has been changed.
+     *
+     * @param to                    The sender.
+     * @param urlWarnChangePassword The URL.
+     */
+    void sendEmailUpdatePassword(String to, String urlWarnChangePassword);
 }
