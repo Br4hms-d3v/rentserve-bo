d83b525e2266e7dd677ee082c755c90d4e846266
Brahms <bramso.brahms@gmail.com>
2026-01-21 00:38:00 +0100

    add method to send a mail for changing password

--- src/main/java/be/brahms/TFE_RentServe/services/email/impl/EmailServiceImpl.java
+++ src/main/java/be/brahms/TFE_RentServe/services/email/impl/EmailServiceImpl.java
@@ -55,4 +55,40 @@ public class EmailServiceImpl implements EmailService {
             System.out.println("Error to send the mail: " + e.getMessage());
         }
     }
+
+    /**
+     * Send a confirmation email for update
+     * if the user doesn't change password.
+     * he clicks on the link to update
+     *
+     * @param to                    The email to send to.
+     * @param urlWarnChangePassword The url to report that the password has been changed
+     */
+    @Override
+    public void sendEmailUpdatePassword(String to, String urlWarnChangePassword) {
+
+        String htmlWarPasswordUpdateHtml =
+                "<html>" +
+                        "<body>" +
+                        "<h1>Changement du passe</h1>" +
+                        "<p>Votre mot de passe à été mise à jour :</p>" +
+                        "<p>Si ce n'est pas vous qui avez modifier votre mot de passe !</p>" +
+                        "<p>Veuillez cliquez sur le lien et modifier votre mot de passe.</p>" +
+                        "<a href=\"" + urlWarnChangePassword + "\">Changer mon mot de passe</a>" +
+                        "</body>" +
+                        "</html>";
+
+        MimeMessage messageConfirmation = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(messageConfirmation, true);
+            helper.setTo(to);
+            helper.setSubject("Confirmation du changement du passe");
+            helper.setText(htmlWarPasswordUpdateHtml, true);
+
+            mailSender.send(messageConfirmation);
+        } catch (Exception e) {
+            System.out.println("erreur lors de l'envoie du mail: " + e.getMessage());
+        }
+    }
 }
