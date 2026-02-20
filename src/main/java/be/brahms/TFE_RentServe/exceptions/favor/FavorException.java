8774939fb828df5300ba278f49f6286b4845ab3a
Brahms <bramso.brahms@gmail.com>
2026-02-20 20:24:17 +0100

    Rename FavourException to FavorException

--- src/main/java/be/brahms/TFE_RentServe/exceptions/favor/FavorException.java
+++ src/main/java/be/brahms/TFE_RentServe/exceptions/favor/FavorException.java
@@ -3,13 +3,13 @@ package be.brahms.TFE_RentServe.exceptions.favor;
 /**
  * This is a general exception for favor errors.
  */
-public class FavourException extends RuntimeException {
+public class FavorException extends RuntimeException {
     /**
      * Create a new favor exception.
      *
      * @param message the error message
      */
-    public FavourException(String message) {
+    public FavorException(String message) {
         super(message);
     }
 }
