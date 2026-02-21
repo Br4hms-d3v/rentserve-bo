9148db07f87d317c2234c019ee6fc79b5e54a40a
Brahms <bramso.brahms@gmail.com>
2026-02-22 00:38:34 +0100

    DTO for favor and updateForm and CreateForm

--- src/main/java/be/brahms/TFE_RentServe/models/dtos/favor/FavorDTO.java
+++ src/main/java/be/brahms/TFE_RentServe/models/dtos/favor/FavorDTO.java
@@ -1,5 +1,12 @@
 package be.brahms.TFE_RentServe.models.dtos.favor;
 
+/**
+ * FavorDTO is a data transfer object for favor information
+ * It holds favor details like name of favor
+ *
+ * @param id        the identifier of the favor
+ * @param nameFavor the name of favor
+ */
 public record FavorDTO(
         Long id,
         String nameFavor
