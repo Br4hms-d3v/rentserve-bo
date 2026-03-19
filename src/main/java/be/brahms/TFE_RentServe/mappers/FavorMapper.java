d103119b5a8d62b8f6230c2ba506fe164975e4e2
Brahms <bramso.brahms@gmail.com>
2026-03-19 01:18:46 +0100

    Add fromCreateFavorForm inside mapper

--- src/main/java/be/brahms/TFE_RentServe/mappers/FavorMapper.java
+++ src/main/java/be/brahms/TFE_RentServe/mappers/FavorMapper.java
@@ -2,6 +2,7 @@ package be.brahms.TFE_RentServe.mappers;
 
 import be.brahms.TFE_RentServe.models.dtos.favor.FavorByIdDTO;
 import be.brahms.TFE_RentServe.models.dtos.favor.FavorDTO;
+import be.brahms.TFE_RentServe.models.forms.favor.FavorFormDTO;
 import be.brahms.TFE_RentServe.models.forms.favor.UpdateFavorFormDTO;
 import be.brahms.TFE_RentServe.models.entities.Favor;
 import org.mapstruct.Mapper;
@@ -54,6 +55,16 @@ public interface FavorMapper {
     List<FavorDTO> toListDto(List<Favor> favors);
 
     // Form to Entity
+
+    /**
+     * Convert CreateFavorForm to a Favor entity.
+     * Used when create a favor
+     *
+     * @param form  the favor form
+     * @return the favor entity
+     */
+    @Mapping(target = "category", ignore = true)
+    Favor fromCreateFavorForm(FavorFormDTO form);
 
     /**
      * Update an existing Favor entity with data from a UpdateFavorForm.
